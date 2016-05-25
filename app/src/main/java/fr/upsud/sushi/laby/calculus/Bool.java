c5da592f8bd78847dc963d5ed5e4ce9c12437c0b
SushiEleonore <eleonore_bartenlian@yahoo.fr>
2016-05-25 13:47:37 +0200

    Added ITerm

--- app/src/main/java/fr/upsud/sushi/laby/calculus/Bool.java
+++ app/src/main/java/fr/upsud/sushi/laby/calculus/Bool.java
@@ -3,7 +3,7 @@ package fr.upsud.sushi.laby.calculus;
 /**
  * Created by proval on 5/25/16.
  */
-public class Bool {
+public class Bool extends ITerm{
 
     public boolean eval(){
         return true;
--- app/src/main/java/fr/upsud/sushi/laby/calculus/Instr.java
+++ app/src/main/java/fr/upsud/sushi/laby/calculus/Instr.java
@@ -3,7 +3,7 @@ package fr.upsud.sushi.laby.calculus;
 /**
  * Created by proval on 5/23/16.
  */
-public class Instr {
+public class Instr extends ITerm {
 
     public void eval(){}
 }
