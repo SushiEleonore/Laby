89f1bc8f30a03824eeffe5151c11c329a14b023b
SushiEleonore <eleonore_bartenlian@yahoo.fr>
2016-06-16 15:22:58 +0200

    hello

--- app/src/main/java/fr/upsud/sushi/laby/utils/Constants.java
+++ app/src/main/java/fr/upsud/sushi/laby/utils/Constants.java
@@ -12,15 +12,21 @@ public class Constants {
 
     //Agrandissement des images
     public static final int SCALE = 4;
+    public static final int IMAGE_SIZE = 14;
 
     //TODO : put the maze in the middle of the frame
 
     public static float getScale(LinearLayout lay, Level l) {
         int hauteur = lay.getHeight();
         int horizontalitude = lay.getWidth();
-        
+        System.out.println("SCAAAALE");
+
+
+
         return 4;
+
     }
+
 
 
     //Add sizeX and sizeY (width and height of the images)
